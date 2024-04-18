fd1356f80c8405f3e12efd7e22e45a574e1bffc4
wgrier-asu <56945057+wgrier-asu@users.noreply.github.com>
2024-04-18 11:22:06 -0700

    impedance force control - incomplete

--- force_control_impedance.m
+++ force_control_impedance.m
@@ -1,13 +1,21 @@
 %% Force Control 
 
 
+%% Impedance Control
+function [y] = impedance_force_control(R, o_d, R_d, od_d, Rd_d, odd_d, Rdd_d, o_e, R_e, od_e, Rd_e, he)
+    % TODO
+end
+
 %% Inverse Dynamics
 % y : joint accelerations
 % he : end-effector generalized forces
 % q : joint positions
 % qd : joint velocities
 function [u] = invdyn(R, y, q, qd, he)
-    u = R.rne(q, qd, y); % compute inverse dynamics
+    qdd = y;
+    grav = (0 0 -9.8)';
+    fext = he;
+    u = R.rne(q, qd, qdd, grav, fext); % compute inverse dynamics
 end
 
 %% Direct Kinematics
@@ -35,3 +43,24 @@ end
 % function [he, q, qd] = manipulator(u)
 %     
 % end
+
+% TODO
+function g = gravity(q)
+    g = q*0;
+end
+
+% PD Controller with Gravity Compensation
+function tau = mytorqfun(t, q, qd, R, xd, P, D)
+    T = R.fkine(q);
+    xe = T(1:3, 4);
+
+    g = gravity(q);
+
+    J = R.jacob0(q, 'eul');
+
+    tau = g + J'*P*(xd-xe) - J'*D*J*qd; 
+end
+
+function [q,qd] = manipulator_model(torqfun)
+    [t,q,qd] = R.fdyn(1, torqfun, R, xd, P, D);
+end
