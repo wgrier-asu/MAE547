e704d09fb6f73a2bc4cfa2a2a9b89550a8c17a7f
wgrier-asu <56945057+wgrier-asu@users.noreply.github.com>
2024-04-18 11:43:36 -0700

    impedance force control function

--- force_control_impedance.m
+++ force_control_impedance.m
@@ -2,8 +2,16 @@
 
 
 %% Impedance Control
-function [y] = impedance_force_control(R, o_d, R_d, od_d, Rd_d, odd_d, Rdd_d, o_e, R_e, od_e, Rd_e, he)
-    % TODO
+function [y] = impedance_control(x_d, xd_d, xdd_d, x_e, xd_e, he, P, D)
+    %hd_e : end-effector forces w.r.t. desired frame
+
+    hd_e = he; % wrong
+
+
+    % y = pinv(J_Ad)*pinv(M_d)*(D*(xd_d-xd_e) + P*(xd-xe) - M_d*Jd_Ad*qd+M_d*bd-hd_e)
+    % M_d*(xdd_d - y) + D*(xd_d-xd_e) + P*(x_d-x_e) = hd_e
+    y = xdd_d - pinv(M_d)*(hd_e - D*(xd_d-xd_e) - P*(x_d-x_e))
+    
 end
 
 %% Inverse Dynamics
@@ -18,20 +26,7 @@ function [u] = invdyn(R, y, q, qd, he, g)
     u = R.rne(q, qd, qdd, grav, fext); % compute inverse dynamics
 end
 
-%% Direct Kinematics
-function [o, R, od, omega] = direct_kinematics(R, q, qd)
-    T = R.fkine(q);
-    o = T(1:3, 4);
-    R = T(1:3, 1:3);
 
-    J = R.jacob0(q);
-    dX = J*qd;
-
-    od = dX(1:3);
-    omega = dX(4:6);
-
-    % dQ/dt = J(q)^-1 dX/dt
-end
 
 %% Manipulator Dynamics
 % Torque to [End-Effector Force, Joint Positions, Joint Velocities]
@@ -63,4 +58,20 @@ end
 
 function [q,qd] = manipulator_model(torqfun)
     [t,q,qd] = R.fdyn(1, torqfun, R, xd, P, D);
+end
+
+
+%% Direct Kinematics
+function [o, R, od, omega] = direct_kinematics(R, q, qd)
+    T = R.fkine(q);
+    o = T(1:3, 4);
+    R = T(1:3, 1:3);
+
+    J = R.jacob0(q);
+    dX = J*qd;
+
+    od = dX(1:3);
+    omega = dX(4:6);
+
+    % dQ/dt = J(q)^-1 dX/dt
 end
